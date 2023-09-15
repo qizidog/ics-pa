8030ec9d6feb3ace07a4a5d43facb1c4c4f9ef31
qizidog <451559450@qq.com>
2023-09-15 12:04:26 +0800

    fix: imm and type of instruction
    1. fix imm caculation of TYPE_B and TYPE_J
    2. fix type of ebreak

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -34,12 +34,12 @@ enum {
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immB() do { \
-  *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (SEXT(BITS(i, 30, 25), 6) << 5)  \
-       | (SEXT(BITS(i, 11, 8), 4) << 1)   | (SEXT(BITS(i, 7, 7), 1) << 11); \
+  *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 30, 25) << 5) \
+       | (BITS(i, 11, 8) << 1)   | (BITS(i, 7, 7) << 11); \
 } while(0)
 #define immJ() do { \
-  *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (SEXT(BITS(i, 30, 21), 10) << 1)  \
-       | (SEXT(BITS(i, 20, 20), 1) << 11) | (SEXT(BITS(i, 19, 12), 8) << 12); \
+  *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 30, 21) << 1) \
+       | (BITS(i, 20, 20) << 11) | (BITS(i, 19, 12) << 12); \
 } while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
@@ -86,7 +86,7 @@ static int decode_exec(Decode *s) {
   // Memory Ordering Instructions
 
   // Environment Call and Breakpoints
-  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , I, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
 
   // Invalid Pattern
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
