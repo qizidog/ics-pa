47362d6cc7486bdeedbdeb0b5bfa442248248690
qizidog <451559450@qq.com>
2023-10-06 20:29:10 +0800

    pa2.3: refactor printf
    1. negtive max value is now acceptable to print
    2. complete all print function with only one macro
    3. complete printf

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -16,142 +16,127 @@ enum {
   PHM_N = '\0', PHM_L = 'l',
 };
 
-static int itostr(char *s, int d) {
-  int ct = 0;
-  int i = 0;
+
+static char* itostr(char *str, int d) {
+  int sz = 0;
+  int dd = d;
+  bool neg = false;
+  bool ovf = false;
+  int ovfid = 0;
 
   if (d < 0) {
-    ++ct;
-    s[i++] = '-';
-    d = -d;  // negative max integer?
+    ++sz;
+    neg = true;
+    ovf = d == -d;  // negtive max
+    d = ovf ? -(d + 1) : -d;
   }
 
-  char *p = s + i;
+  // count strlen
+  dd = d;
   do {
-    s[i++] = d % 10 + 48;
-    d /= 10;
-    ++ct;
-  } while (d != 0);
-  s[i] = '\0';
+    ++sz;
+    dd /= 10;
+  } while (dd != 0);
+  /* char * str = (char *) malloc(sz + 1); */
+  str[sz] = '\0';
+  ovfid = sz - 1;
 
-  // swap number order
-  char *q = s + i - 1;
-  char t;
-  while (p < q) {
-    t = *p;
-    *p++ = *q;
-    *q-- = t;
+  // build string
+  dd = d;
+  do {
+    str[--sz] = '0' + dd % 10;
+    dd /= 10;
+  } while (dd != 0);
+  if (neg) str[--sz] = '-';
+  if (ovf) {
+    assert(str[ovfid] != '9');
+    str[ovfid] = str[ovfid] + 1;
   }
 
-  return ct;
+  return str;
 }
 
-// parse the placeholder pointed to by p,
-// concat parsed string to *d with next param in args,
-// and update d to '\0', p to next char to parse.
-#define parse_ph(d, p, args) do { \
-  assert(*(p) == '%'); \
-  ++(p); \
-  int parsed_int_val; \
-  char* parsed_str_val; \
-  switch (*(p)) { \
-    case PHT_C: \
-      parsed_int_val = (char) va_arg(args, int); \
-      (d)[0] = parsed_int_val; \
-      (d)[1] = '\0'; \
-      ++(d); \
-      break; \
-    case PHT_S: \
-      parsed_str_val = va_arg(args, char*); \
-      strcat((d), parsed_str_val); \
-      (d) += strlen(parsed_str_val); \
-      break; \
-    case PHT_D: \
-      parsed_int_val = (char) va_arg(args, int); \
-      (d) += itostr((d), parsed_int_val); \
-      break; \
-    case PHT_P: \
-      (d)[0] = '%'; \
-      (d)[1] = '\0'; \
-      ++(d); \
-      break; \
-    default: assert(0); \
+#define PARSE_FMT(cond_and, ...) while (*p != '\0' && (cond_and)) { \
+  if (*p != '%') { \
+    __parsed_char = *p++; \
+    __VA_ARGS__; \
+    ++ret; \
+    continue; \
   } \
-  ++(p); \
-} while(0)
-
-#define parse_nph(d, p, n, args) do { \
-  assert(*(p) == '%'); \
-  assert((n) > 0); \
-  ++(p); \
-  int parsed_int_val; \
-  char* parsed_str_val; \
-  switch (*(p)++) { \
+  switch (*++p) { \
+    case PHT_P: \
+      __parsed_char = '%'; \
+      __VA_ARGS__; \
+      ++ret; \
+      break; \
     case PHT_C: \
-      parsed_int_val = (char) va_arg(args, int); \
-      (d)[0] = parsed_int_val; \
-      (d)[1] = '\0'; \
-      ++(d); \
+      __parsed_int = va_arg(ap, int); \
+      __parsed_char = (char)__parsed_int; \
+      __VA_ARGS__; \
+      ++ret; \
       break; \
     case PHT_S: \
-      parsed_str_val = va_arg(args, char*); \
-      strncat((d), parsed_str_val, (n) - 1); \
-      if (strlen(parsed_str_val) >= (n) - 1) { \
-        d += (n); \
-        (p) = (d); /* set *p to '\0' */ \
-      } else { \
-        d += (strlen(parsed_str_val)); \
+      __parsed_str = va_arg(ap, char *); \
+      __parsed_int = strlen(__parsed_str); \
+      for (int i = 0; i < __parsed_int; i++) { \
+        __parsed_char = __parsed_str[i]; \
+        __VA_ARGS__; \
+        ++ret; \
+        if (!(cond_and)) break; \
       } \
       break; \
     case PHT_D: \
-      parsed_int_val = (char) va_arg(args, int); \
-      { /* check usable space */ \
-        int ct = 0, t = parsed_int_val; \
-        if (t < 0) { ++ct; t = -t; } \
-        do { \
-          t /= 10; \
-          ++ct; \
-        } while (t != 0); \
-        if (ct > n) { \
-          (p) = (d); /* set *p to '\0' */ \
-          break; \
-        } \
+      __parsed_int = va_arg(ap, int); \
+      __parsed_str = buff; \
+      itostr(__parsed_str, __parsed_int); \
+      __parsed_int = strlen(__parsed_str); \
+      for (int i = 0; i < __parsed_int; i++) { \
+        __parsed_char = __parsed_str[i]; \
+        __VA_ARGS__; \
+        ++ret; \
+        if (!(cond_and)) break; \
       } \
-      (d) += itostr((d), parsed_int_val); \
       break; \
-    case PHT_P: \
-      (d)[0] = '%'; \
-      (d)[1] = '\0'; \
-      ++(d); \
+    default: \
+      assert(0); \
       break; \
-    default: assert(0); \
   } \
-} while(0)
+  ++p; \
+}
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  // pointer to the next char to copy
+  const char * p = fmt;
+  int ret = 0;
+
+  char buff[32];  // TODO: use malloc instead
+  int __parsed_int = 0;
+  char * __parsed_str = buff;
+  char __parsed_char = '\0';
+
+  va_list ap;
+  va_start(ap, fmt);
+  PARSE_FMT(true, putch(__parsed_char));
+  va_end(ap);
+
+  return ret;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  *out = '\0';
+  // pointer to the next char to copy
+  const char * p = fmt;
+  int ret = 0;
+
+  char buff[32];  // TODO: use malloc instead
+  int __parsed_int = 0;
+  char * __parsed_str = buff;
+  char __parsed_char = '\0';
+
   char *d = out;  // pointer to '\0'
-  // p1 pointer to the first char to copy, p2 pointer to the next char to copy
-  const char *p1 = fmt, *p2 = fmt;
+  PARSE_FMT(true, *d++ = __parsed_char );
+  out[ret] = '\0';
 
-  while (*p2 != '\0') {
-    if (*p2 == '%') {
-      strncat(d, p1, p2 - p1);
-      d += p2 - p1;
-      parse_ph(d, p2, ap);  // p2 and d are updated
-      p1 = p2;
-    } else {
-      ++p2;
-    }
-  }
-  strncat(d, p1, p2 - p1);
-  d += p2 - p1;
-
-  return d - out;
+  return ret;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
@@ -165,21 +150,20 @@ int sprintf(char *out, const char *fmt, ...) {
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  *out = '\0';
+  // pointer to the next char to copy
+  const char * p = fmt;
+  int ret = 0;
+
+  char buff[32];  // TODO: use malloc instead
+  int __parsed_int = 0;
+  char * __parsed_str = buff;
+  char __parsed_char = '\0';
+
   char *d = out;  // pointer to '\0'
-  // p pointer to the next char to copy
-  const char *p = fmt;
+  PARSE_FMT(ret < n - 1,  *d++ = __parsed_char );
+  out[ret] = '\0';
 
-  while (*p != '\0' && d - out < n - 1) {
-    if (*p == '%') {
-      parse_nph(d, p, n, ap);
-    } else {
-      *d++ = *p++;
-    }
-  }
-  *d = '\0';
-
-  return d - out;
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
@@ -192,6 +176,6 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
   return ret;
 }
 
-#undef parse_ph
+#undef PARSE_FMT
 
 #endif
